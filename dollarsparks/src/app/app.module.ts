3fafe5dba753b065f638be659a9866b3a85f2dd7
Matin Adebayo <matin.adebayo@avrion.co.uk>
2024-05-03 16:10:18 +0100

    deploy

--- dollarsparks/src/app/app.module.ts
+++ dollarsparks/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavHeaderComponent } from './pages/nav-header/nav-header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavHeaderComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
